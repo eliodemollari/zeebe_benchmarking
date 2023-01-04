f136f464ab802d728208d75202eb1270a9f1adc4
Elio Demollari <elio.demollari@gmail.com>
2023-01-04 18:43:13 +0100

    Start multiple processes

--- script.js
+++ script.js
@@ -12,13 +12,22 @@ void (async () => {
     const zbc = new ZB.ZBClient() // localhost:26500 || ZEEBE_GATEWAY_ADDRESS
 
     // Deploy a bpmn process
-    const deploy_process = await zbc.deployProcess('./processes/bug_reported.bpmn')
+    const deploy_process = await zbc.deployProcess(['./processes/online_ticket_purchase.bpmn', './processes/bug_reported.bpmn'])
     console.log(deploy_process)
 
-    // Starting nine instances of that process
+    // Starting nine instances of bug_reported.bpmn
     for (let i = 1; i < 10; i++){
         console.log('Start instance no. ', i)
         const start_instance = await zbc.createProcessInstance('Process_1hzmijf', {
+            testData: 'something',
+        })
+        console.log(start_instance)
+    }
+
+    // Starting nine instances of online_ticket_purchase.bpmn
+    for (let i = 1; i < 10; i++){
+        console.log('Start instance no. ', i)
+        const start_instance = await zbc.createProcessInstance('Process_1mqc2bt', {
             testData: 'something',
         })
         console.log(start_instance)
