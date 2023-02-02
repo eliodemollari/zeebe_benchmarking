96279946f4d3b9abd62efc1481e5776d346d6a95
Elio Demollari <elio.demollari@gmail.com>
2023-02-02 11:32:12 +0100

    Add grafata&prometheus config

--- zeebe_client_complex.js
+++ zeebe_client_complex.js
@@ -12,13 +12,13 @@ void (async () => {
     const zbc = new ZB.ZBClient() // localhost:26500 || ZEEBE_GATEWAY_ADDRESS
 
     // Deploy a bpmn process
-    const deploy_process = await zbc.deployProcess( './processes/bug_reported.bpmn')
+    const deploy_process = await zbc.deployProcess('./processes/online_ticket_purchase.bpmn')
     console.log(deploy_process)
 
-    // Starting nine instances of bug_reported.bpmn
+    // Starting nine instances of online_ticket_purchase.bpmn
     while (true){
         console.log('Deploying instances. ')
-        const start_instance = await zbc.createProcessInstance('Process_1hzmijf', {
+        const start_instance = await zbc.createProcessInstance('Process_1mqc2bt', {
             testData: 'something',
         })
         console.log(start_instance)
